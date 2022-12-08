871b9d00424014a1399829debac73234e065aa96
jraysilva <raymundo.silva@concir.com>
2022-12-08 01:30:27 -0600

    update ruta

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Menu setToken={setToken} />
       <HashRouter>
       <Routes>
-          <Route path="/productos" exact element={token ?  <inicio  />  : <Login token={token} setToken={setToken}/>}/>
+          <Route path="/productos" exact element={token ?  <Inicio  />  : <Login token={token} setToken={setToken}/>}/>
           <Route exact path="/" element={token ?  <Productos />  : <Login token={token} setToken={setToken}/>}/>
       </Routes>
       </HashRouter>
